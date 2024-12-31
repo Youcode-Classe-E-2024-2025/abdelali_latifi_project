ce0eebaa43564ee1ac7868033ff6ce97b54617e8
latifi777 <abdelalilatifi3@gmail.com>
2024-12-31 12:42:17 +0100

    connected with database

---
+++ backoffice/config/connexion.php
@@ -0,0 +1,30 @@
+<?php
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "project";
+    private $username = "root";
+    private $password = "";
+    private $connexion;
+
+    public function getConnection() {
+        $this->connexion = null;
+
+        try {
+            $this->connexion = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            //echo "Connection successful!<br>";
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage() . "<br>";
+        }
+
+        return $this->connexion;
+    }
+}
+
+
