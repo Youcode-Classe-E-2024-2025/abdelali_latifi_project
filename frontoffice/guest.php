b1f347a0b2bc269c64105b681bc4e14bf2e840a0
latifi777 <abdelalilatifi3@gmail.com>
2025-01-09 11:54:43 +0100

    The security implementation on guest page

--- frontoffice/guest.php
+++ frontoffice/guest.php
@@ -1,3 +1,18 @@
+<?php 
+session_start();
+require_once '../backoffice/config/connexion.php';
+require_once '../backoffice/controlers.php/project.php';
+
+!isset($_SESSION['role']) || $_SESSION['role'] !== 'team_member' ? header('Location: index.php') : '';
+
+$db = new Database();
+$conn = $db->getConnection();
+$projectManager = new Project($conn);
+
+$projects = $projectManager->getAllProjects();
+$tasks = $projectManager->getAllTasks();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +22,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 font-sans">
-   <h1>DIMA Wydade</h1>
+   <h1>okaay</h1>
 
 </body>
 </html>
